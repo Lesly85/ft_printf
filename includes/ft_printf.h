bedef01b6fbf6f7ca2077b54442436c6c3726015
Lesly Escobar <lesly.escobar.morales@gmail.com>
2025-01-07 22:35:16 +0000

    inicio

---
+++ includes/ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 22:27:09 by codespace         #+#    #+#             */
+/*   Updated: 2025/01/07 22:34:39 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdlib.h>
--- makefile
+++ makefile
@@ -3,16 +3,16 @@
 #                                                         :::      ::::::::    #
 #    makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: lesescob <lesescob@student.42.fr>          +#+  +:+       +#+         #
+#    By: codespace <codespace@student.42.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/20 20:42:55 by lesescob          #+#    #+#              #
-#    Updated: 2024/12/20 20:56:08 by lesescob         ###   ########.fr        #
+#    Updated: 2025/01/07 22:34:39 by codespace        ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CC = cc
 flags = -Wall -Werror -Wextra
-NAME = libft.a
+NAME = libftprintf.a
 SRC =
 
 OBJECTS = ${SRC: .c=.o}
