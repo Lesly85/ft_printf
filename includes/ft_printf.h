2828815608df5e73ab2f269a4d86f6bfaf0c1e42
Lesly Escobar <lesly.escobar.morales@gmail.com>
2025-01-29 19:16:22 +0100

    aggiornamento

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: lesescob <lesescob@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 22:27:09 by codespace         #+#    #+#             */
-/*   Updated: 2025/01/07 22:34:39 by codespace        ###   ########.fr       */
+/*   Updated: 2025/01/29 17:49:21 by lesescob         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,9 @@
 # include <stdarg.h>
 # include <stddef.h>
 # include <stdlib.h>
+
+int		ft_putchar(char c);
+size_t	ft_putstr(char *str);
+int		ft_printf(const char *operator, ...);
+
+#endif
